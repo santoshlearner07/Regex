bc32c8f20b8e2c13d98b21a99514b383e3220be4
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-18 15:58:16 +0530

    Last Name Added

--- src/com/company/Regex.java
+++ src/com/company/Regex.java
@@ -11,14 +11,19 @@ public class Regex {
         /*
         First name entered
          */
-
         Pattern pattern = Pattern.compile("[A-Za-z]{3,}");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First name -> ");
         String firstName = sc.next();
         Matcher matcher = pattern.matcher(firstName);
+/*
+Last name added
+ */
+        System.out.println("Enter Last name -> ");
+        String lastName = sc.next();
+        Matcher matcher2 = pattern.matcher(lastName);
 
-        if (matcher.find())
+        if (matcher.find() && matcher.find())
             System.out.println("Details Matched");
         else
             System.out.println("Details not matched");
