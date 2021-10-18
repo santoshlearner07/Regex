bccd83d98806a8b9b1f314908953f2378b6cc555
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-18 15:54:10 +0530

    First Name added

--- src/com/company/Regex.java
+++ src/com/company/Regex.java
@@ -1,10 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Regex {
     public static void main(String[] args) {
 
-        System.out.println("Solving Regex");
+        System.out.println("Entering Details");
+        /*
+        First name entered
+         */
+
+        Pattern pattern = Pattern.compile("[A-Za-z]{3,}");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First name -> ");
+        String firstName = sc.next();
+        Matcher matcher = pattern.matcher(firstName);
+
+        if (matcher.find())
+            System.out.println("Details Matched");
+        else
+            System.out.println("Details not matched");
+
 
     }
 }
